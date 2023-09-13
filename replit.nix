76577c83e305c9ce904bfd1368088fed897cb5fc
Replit user <>
2023-09-13 05:50:52 +0000

    add webp format and tags

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.fetchutils
+    pkgs.libwebp
+    pkgs.libwebp
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server  
   ];
